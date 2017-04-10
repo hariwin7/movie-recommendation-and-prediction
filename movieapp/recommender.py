3fc9e9ff2a4c8e999a7ffbbb2f71d0efdc70cd8f
hariwin7 <hariwin7@gmail.com>
2017-04-10 17:43:24 +0530

    small change in recommender.py

--- movieapp/recommender.py
+++ movieapp/recommender.py
@@ -15,16 +15,16 @@ def recommend(userid=None):
     actions= gl.SFrame.from_sql(conn, "SELECT * FROM movieapp_ratings")
 
     training_data, validation_data = gl.recommender.util.random_split_by_user(actions, 'userid', 'movieid')
-    model = gl.item_similarity_recommender.create(training_data, user_id='userid', item_id='movieid', target='rating', similarity_type='pearson')
-    # model = gl.recommender.create(training_data, 'userid', 'movieid')
+    # model = gl.item_similarity_recommender.create(training_data, user_id='userid', item_id='movieid', target='rating', similarity_type='pearson')
+    model = gl.recommender.create(training_data, 'userid', 'movieid')
     # results = model.recommend([int(userid)])
     if userid:
-        results = model.recommend([int(userid)],k=6)
+        results = model.recommend([int(userid)],k=9)
     testrec = model.recommend()
     print precision_recall_by_user(validation_data, testrec, cutoffs=[5, 10])
     model.save("my_model")
     return results
 def loadmodel(userid):
     model = gl.load_model("my_model")
-    results = model.recommend([int(userid)],k=6)
+    results = model.recommend([int(userid)],k=9)
     return results
